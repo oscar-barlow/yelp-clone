fbd93cacd2bed14cfa239a93b4460c954cb0336d
Dines Poulsen <dinespoulsen84@gmail.com>
2017-01-09 13:43:26 +0000

    Can add a restaurant and lists all restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -10,15 +10,26 @@ feature 'restaurants' do
     end
   end
 
-  # context 'restaurants have been added' do
-  #   before do
-  #     Restaurant.create('KFC')
-  #   end
-  #
-  #   scenario 'display restaurants' do
-  #     visit '/restaurants'
-  #     expect(page).to have_content('KFC')
-  #     expect(page).no_to have_content 'No restaurants yet'
-  #   end
-  # end
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+  end
+
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit "/restaurants"
+      click_link "Add a restaurant"
+      fill_in "restaurant[name]", with: "KFC"
+      click_button "Create Restaurant"
+      expect(page).to have_content "KFC"
+      expect(current_path).to eq "/restaurants"
+    end
+  end
 end
