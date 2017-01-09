492bc9df7ca103e8248f77fd5422e4641d27b566
Dines Poulsen <dinespoulsen84@gmail.com>
2017-01-09 16:33:15 +0000

    Added review model

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+
+  def new
+    p params
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    restaurant = Restaurant.find(params[:restaurant_id])
+    restaurant.reviews.create(review_params)
+    redirect_to restaurants_path
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :thoughts)
+  end
+end
