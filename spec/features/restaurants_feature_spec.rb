10c8d6ef952637d9bdb5345219dec26f1cbf46ce
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-09 11:42:35 +0000

    app has Restaurants resource routes, haml views

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,10 +3,22 @@ require 'rails_helper'
 feature 'restaurants' do
   context 'no restaurants have been added' do
 
-    scenario 'should deisplay a prompt to add a restaurant' do
+    scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  # context 'restaurants have been added' do
+  #   before do
+  #     Restaurant.create('KFC')
+  #   end
+  #
+  #   scenario 'display restaurants' do
+  #     visit '/restaurants'
+  #     expect(page).to have_content('KFC')
+  #     expect(page).no_to have_content 'No restaurants yet'
+  #   end
+  # end
 end
