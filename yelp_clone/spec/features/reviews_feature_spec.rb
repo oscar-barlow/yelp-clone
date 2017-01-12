9aa412a5ac0d298fb1a67d78284c44e260efcfb4
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-12 16:14:55 +0000

    Create a restaurant with a user id for test

--- yelp_clone/spec/features/reviews_feature_spec.rb
+++ yelp_clone/spec/features/reviews_feature_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
 feature 'reviewing' do
-  before { Restaurant.create name: 'KFC' }
+  before do
+    sign_up
+    visit '/restaurants'
+    click_link 'Add a restaurant'
+    fill_in 'Name', with: 'KFC'
+    fill_in 'Description', with: 'Deep fried goodness'
+    click_button 'Create Restaurant'
+  end
 
   scenario 'allows users to leave a review using a form' do
-     visit '/restaurants'
-     click_link 'Review KFC'
-     fill_in "Thoughts", with: "so so"
-     select '3', from: 'Rating'
-     click_button 'Leave Review'
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
 
-     expect(current_path).to eq '/restaurants'
-     expect(page).to have_content('so so')
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content('so so')
   end
 
 end
